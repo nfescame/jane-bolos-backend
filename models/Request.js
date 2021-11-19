d8073b31e3acb927c827a43053fd0150aa1c56cf
nilton <nfescame@gmail.comq:qclear>
2021-11-19 11:29:30 -0300

    request model done

--- models/Request.js
+++ models/Request.js
@@ -4,6 +4,7 @@ const RequestSquema = new Schema({
   products: { type: Array, default: [], required: true },
   totalValue: { type: Number, requered: true },
   clientName: { type: String, required: true },
+  done: { type: Boolean, default: false },
 });
 
 const RequestModel = model("Request", RequestSquema);
